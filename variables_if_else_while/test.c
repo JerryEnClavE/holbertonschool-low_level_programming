b07273a4d45205b0e721ad1e8e28691678ea6700
Jerry Meletiche <jerrymeletiche123@gmail.com>
2024-06-21 09:28:25 -0400

    1-alphabet

---
+++ variables_if_else_while/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*
+*Num:resta A-B
+*Return:always ()
+*
+*/
+
+int resta(int a, int b){
+
+return (a - b);
+}
+
+int main(){
+
+int resultado;
+
+resultado = resta(10,5);
+printf("%d\n", resultado);
+return(0);
+}
