515bc013f9ee4ac5ded06f1e97602f553465a0c4
Jerry Meletiche <jerrymeletiche123@gmail.com>
2024-06-20 23:09:59 -0400

    good

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
-"main.h"
+#include "main.h"
 /**
- * print_alphabet - function that prints the alphabet
+ * print_alphabet - function to print abc
  *
- * Return: Always 0.
+ * Return: 0
  */
-void print_alphabet(void) 
+
+void print_alphabet(void)
 
 {
-   
 	char c;
 
-	for (c = 'a'; c<= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
 	}
 	_putchar('\n');
-}    
+}
