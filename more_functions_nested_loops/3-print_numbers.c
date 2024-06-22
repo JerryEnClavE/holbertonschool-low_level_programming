e92696b4ad9cdeaf72fb6f224f7cc9fd39ac4c98
Jerry Meletiche <jerrymeletiche123@gmail.com>
2024-06-21 23:22:16 -0400

    hoo

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,12 @@
 /**
  * print_numbers - Prints the numbers from 0-9.
  */
-void print_numbers(void)
 {
-	int num;
+    int i;
 
-	for (num = 0; num <= 9; num++)
-	_putchar((num % 10) + '0');
-
-	_putchar('\n');
+    for (i = 0; i < 10; i++)
+    {
+        _putchar(i + '0');
+    }
+    _putchar('\n');
 }
