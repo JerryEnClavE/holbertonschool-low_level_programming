7d09914b35003891b3f185f3dabe357cea80c28a
Jerry Meletiche <jerrymeletiche123@gmail.com>
2024-06-26 15:42:29 -0400

    hola

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,11 +11,18 @@ char *_strcat(char *dest, char *src)
 	int j;
 	int i;
 
-	
-	for (j = 0; dest[j] != '\0'; j++);
-	
-	for (i = 0; src[i] != '\0'; i++);
-	
-	dest[j + i] = src[i];
 
+	for(j = 0; dest[j] != '\0'; j++)
+	{
+		continue;
+	}
+
+	for(i = 0; src[i] != '\0'; i++)
+	{
+		dest[j] = src[i];
+		j++;
+	}
+	dest[j] = '\0';
+	
 	return (dest);
+}
