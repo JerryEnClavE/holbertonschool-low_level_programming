d5b31d96a0d99c69a5e875d52a4759fcf5ef2f53
Jerry Meletiche <jerrymeletiche123@gmail.com>
2024-06-26 20:55:01 -0400

    hola

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strncpy - concatenate strings, defining the size of second string.
  * @dest: string with concatenation
  * @src: string to be concatenated
  * @n: size of second string
+ *
  * Return: Always 0.
  */
 char *_strncpy(char *dest, char *src, int n)
@@ -14,11 +14,11 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{	
-		dest [i] = src [i];
+		dest[i] = src[i];
 	}
 	for ( ; i < n; i++)
 	{
-		dest [i] = '\0';
+		dest[i] = '\0';
 	}
 	return (dest);
 }
