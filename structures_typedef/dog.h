1bb23ae187b2399062687ce689b427a1d1119100
Jerry Meletiche <jerrymeletiche123@gmail.com>
2024-07-09 13:17:00 -0400

    hola

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,19 @@
-#ifndef "DOG_H"
-#define "DOG_H"
-
-type struct dog
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - header file
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of de dog owner
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
-	char * owner;
-} dog-t;
+	char *owner;
+}
+
+dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
