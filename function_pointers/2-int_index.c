6ba91e55c79084fc88d3d44b85e3db7bbd3719ae
Jerry Meletiche <jerrymeletiche123@gmail.com>
2024-07-12 12:32:34 -0400

    hola

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - funcion that searches an integer
+ *
+ * @array: first value
+ * @size: function
+ * @cmp: function
+ *
+ * Return: value
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int value = 0, i;
+
+	if (size <= 0)
+	{
+	return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		value = cmp(array[i]);
+		if (value == 1)
+			return (i);
+	}
+	return (-1);
+}
