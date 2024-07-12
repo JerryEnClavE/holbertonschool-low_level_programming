ebdb79c79ca1d32232495c1e19417ec1782ed1d9
Jerry Meletiche <jerrymeletiche123@gmail.com>
2024-07-12 12:41:54 -0400

    hola

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -14,10 +14,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int value = 0, i;
 
-	if (size <= 0)
-	{
-	return (-1);
-	}
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	
 	for (i = 0; i < size; i++)
 	{
 		value = cmp(array[i]);
