6d79fc63addc85a034b963eeba0564daaed70bd6
Jerry Meletiche <jerrymeletiche123@gmail.com>
2024-07-13 00:42:21 -0400

    Descripción de los cambios realizados

---
+++ argc_argv/practica.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main ()
+{
+int n1 ,n2 ,sum;
+
+printf("introduce el primer numero\n" );
+scanf("%i", &n1);
+printf("introduce el segundo numero\n" );
+scanf("%i", &n2);
+
+sum = n1 + n2;
+
+printf("el resultado %i\n",sum);
+
+return 0;
+}
